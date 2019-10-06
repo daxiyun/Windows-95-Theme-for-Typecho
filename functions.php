dc33523a2a708a27bcd3b3a2bade06c4c149fa44
vitoland <admin@liyunbin.com>
2019-10-07 07:42:48 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -38,7 +38,7 @@ function themeConfig($form) {
     $tongji = new Typecho_Widget_Helper_Form_Element_Textarea('tongji', NULL, $config["tongji"], _t('统计代码'), _t('上面的代码会在页面最底部加载，你可以添加一些其它 JS 脚本扩展功能。<br>需要使用 &lt;script&gt;&lt;/script&gt; 标签。'));
     $form->addInput($tongji);
     
-    $instantclick = new Typecho_Widget_Helper_Form_Element_Radio('instantclick', array('true' => '开启', 'false' => '关闭'), "false", _t('开启 InstantClick 无刷新加载，你的博客没有音乐播放器的话不需要开启。'), '请注意：开启此功能后，必须关闭 设置 - 评论 中的 开启反垃圾保护 ，不关闭无法评论。<br>因为我的博客通常没啥人评论。。。所以我懒得修了，实在太麻烦。');
+    $instantclick = new Typecho_Widget_Helper_Form_Element_Radio('instantclick', array('true' => '开启', 'false' => '关闭'), "false", _t('开启 InstantClick 无刷新加载，你的博客没有音乐播放器的话不需要开启。'), '');
     $form->addInput($instantclick);
     
     $JSload = new Typecho_Widget_Helper_Form_Element_Textarea('JSload', NULL, $config["JSload"], _t('InstantClick 重载'), _t('开启 InstantClick 之后，如果某些插件的前台脚本位于 head 区域，可能会导致脚本失效。<br>请在这里填写相关重载函数，具体函数请咨询相关插件作者。<br>不需要 &lt;script&gt;&lt;/script&gt; 标签，直接填写执行函数即可。'));
